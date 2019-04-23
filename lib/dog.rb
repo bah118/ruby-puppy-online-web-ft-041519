eba86fb480b2050135a7541a96a1ed40f6e29df4
BethAnn Hanlon <herbivore137@gmail.com>
2019-04-23 08:12:08 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << name  
+  end
+  
+  def self.all
+    #@@all.each {|x| puts x}
+    puts @@all 
+  end
+  
+  def self.clear_all
+    @@all.clear 
+  end
+  
+end
